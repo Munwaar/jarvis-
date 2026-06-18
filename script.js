3a14cad311aa4a963f7ab4dcc093267778ea736a
Munwaar <gamingcinderace@gmail.com>
2026-06-18 21:13:33 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,72 @@
+function sendMessage(){
+
+    let input =
+    document.getElementById("userInput");
+
+    let message =
+    input.value.toLowerCase();
+
+    let chat =
+    document.getElementById("chat-box");
+
+    chat.innerHTML +=
+    `<div class="user">
+    You: ${message}
+    </div>`;
+
+    let reply = "";
+
+    if(message.includes("hello")){
+        reply = "Hello Munwaar!";
+    }
+
+    else if(message.includes("time")){
+        reply =
+        new Date().toLocaleTimeString();
+    }
+
+    else if(message.includes("date")){
+        reply =
+        new Date().toDateString();
+    }
+
+    else if(message.includes("youtube")){
+        window.open(
+        "https://youtube.com",
+        "_blank"
+        );
+
+        reply =
+        "Opening YouTube...";
+    }
+
+    else if(message.includes("google")){
+        window.open(
+        "https://google.com",
+        "_blank"
+        );
+
+        reply =
+        "Opening Google...";
+    }
+
+    else if(message.includes("who are you")){
+        reply =
+        "I am Jarvis, your AI assistant.";
+    }
+
+    else{
+        reply =
+        "Sorry, I don't understand.";
+    }
+
+    chat.innerHTML +=
+    `<div class="bot">
+    Jarvis: ${reply}
+    </div>`;
+
+    input.value="";
+
+    chat.scrollTop =
+    chat.scrollHeight;
+}
